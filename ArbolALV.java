f4870df0fd22b1738625984889f9d55db9203db3
ShofiSo <kelly.rabanales@gmail.com>
2025-05-12 20:27:52 -0600

    Tercer Commit
    Ya defini la clase ALVTree, sus lados y su altura, vamos bien...

--- ArbolALV.java
+++ ArbolALV.java
@@ -7,3 +7,32 @@ class Node {
         altura = 1;
     }
 }
+
+class AVLTree {
+    Node raiz;
+
+    int getAltura(Node nodo) {
+        return nodo == null ? 0 : nodo.altura;
+    }
+
+    int getFactorBalance(Node nodo) {
+        return nodo == null ? 0 : getAltura(nodo.izquierda) - getAltura(nodo.derecha);
+    }
+
+    int max(int a, int b) {
+        return a > b ? a : b;
+    }
+
+    Node rotarDerecha(Node y) {
+        Node x = y.izquierda;
+        Node T2 = x.derecha;
+
+        x.derecha = y;
+        y.izquierda = T2;
+
+        y.altura = max(getAltura(y.izquierda), getAltura(y.derecha)) + 1;
+        x.altura = max(getAltura(x.izquierda), getAltura(x.derecha)) + 1;
+
+        return x;
+    }
+}
